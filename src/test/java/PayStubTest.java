90c58859f2b3e58c7f4b29d7cee1118f9a3bc087
idareogu <ifeareogunn@gmail.com>
2025-02-10 23:54:46 -0800

    "TestFiles"

--- src/test/java/PayStubTest.java
+++ src/test/java/PayStubTest.java
@@ -1,28 +1,33 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import student.PayStub;
 
-class PayStubTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-    @Test
-    void getEmployeeName() {
+public class PayStubTest {
+
+
+    private PayStub payStub;
+
+    @BeforeEach
+    public void setUp() {
+        payStub = new PayStub("John Doe", 5000.00, 1000.00, 100000.00, 20000.00);
     }
 
     @Test
-    void getPay() {
+    public void testGetterMethods(){
+            assertEquals("John Doe", payStub.getEmployeeName());
+            assertEquals(5000.00, payStub.getPay());
+            assertEquals(1000.00, payStub.getTaxesPaid());
+            assertEquals(100000.00, payStub.getYtdEarnings());
+            assertEquals(20000.00, payStub.getYtdTaxesPaid());
     }
 
     @Test
-    void getTaxesPaid() {
+    public void testToCSV() {
+        String expectedCSV = " John Doe, 5000.00, 1000.00, 100000.00, 20000.00";
+        assertEquals(expectedCSV, payStub.toCSV());
     }
 
-    @Test
-    void getYtdEarnings() {
-    }
 
-    @Test
-    void getYtdTaxesPaid() {
-    }
-
-    @Test
-    void toCSV() {
-    }
 }
